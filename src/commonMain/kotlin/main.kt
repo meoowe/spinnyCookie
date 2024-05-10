0c4a381148ec02db5784d6ba53e4e7bc77608fb0
meoowe <96916064+meoowe@users.noreply.github.com>
2024-05-10 19:11:26 +0100

    :P

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -7,6 +7,7 @@ import korlibs.image.color.*
 import korlibs.image.format.*
 import korlibs.io.file.std.*
 import korlibs.korge.input.*
+import korlibs.korge.view.align.*
 import korlibs.math.geom.*
 import korlibs.math.interpolation.*
 
@@ -20,17 +21,27 @@ class MyScene : Scene() {
     private suspend fun wait(ms: TimeSpan) {
         delay(ms)
     }
-	override suspend fun SContainer.sceneMain() {
-		val image = image(resourcesVfs["cookie.png"].readBitmap()) {
-			anchor(.5, .5)
-			scale(0.8)
-			position(256, 256)
-		}
+
+    override suspend fun SContainer.sceneMain() {
+        val image = image(resourcesVfs["cookie.png"].readBitmap()) {
+            anchor(.5, .5)
+            scale(0.8)
+            position(256, 256)
+        }
+        var cookies = 0
         var rotation = 1.degrees
-        image.onClick { rotation = 4.degrees }
-		while (true) {
+        val cookie = image(resourcesVfs["cookie.png"].readBitmap()) {
+            stage?.let { alignTopToTopOf(it) }
+            stage?.let { alignLeftToLeftOf(it) }
+            scale(0.05)
+        }
+        val cookieDisplay = text(" = $cookies") {
+            alignLeftToRightOf(cookie)
+        }
+        image.onClick { rotation += 1.degrees; cookies += 1 ; cookieDisplay.text = " = $cookies"}
+        while (true) {
             wait(15.milliseconds)
             image.rotation += rotation
-		}
-	}
+        }
+    }
 }
